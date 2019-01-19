0dbf9d5c173de7cbab3163d2fd819f0fba1b7106
Hector Gonzalez <raftb.gonzalez@outlook.com>
2019-01-19 17:26:39 +0000

    test

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -23,7 +23,7 @@ bool load(const char *dictionary)
 unsigned int size(void)
 {
     // TODO
-    return 0;
+     return 0;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
